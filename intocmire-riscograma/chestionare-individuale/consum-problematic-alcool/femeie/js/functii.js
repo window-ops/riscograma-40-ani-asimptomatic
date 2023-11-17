f29e158d63f2fb2cb021e2e65760dfd50b6fb881
window-ops <vladbruma2020@gmail.com>
2023-11-17 20:30:41 +0000

    Update 15 files
    - /css/stil.css
    - /despre/index.html
    - /intocmire-riscograma/index.html
    - /intocmire-riscograma/chestionare-individuale/index.html
    - /intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/index.html
    - /intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/barbat/index.html
    - /intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/barbat/js/functii.js
    - /intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/femeie/index.html
    - /intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/femeie/js/functii.js
    - /intocmire-riscograma/chestionare-individuale/heartscore-score/index.html
    - /intocmire-riscograma/chestionare-individuale/heartscore-score/js/functii.js
    - /intocmire-riscograma/chestionare-individuale/screening-depresie/index.html
    - /intocmire-riscograma/chestionare-individuale/screening-depresie/js/functii.js
    - /intocmire-riscograma/js/functii.js
    - /Ordin_MS_CNAS_1857_441_2023.pdf

---
+++ intocmire-riscograma/chestionare-individuale/consum-problematic-alcool/femeie/js/functii.js
@@ -0,0 +1,46 @@
+let punctaj = 0;
+let intrebaricomplete = 0;
+let nrintrebari = 3;
+let sex = "Femeie";
+let rezultat = "";
+
+function raspunde(idintrebare, adaugarepunctaj) {
+  // Adaugă punctajul și marcheaza o întrebare ca fiind completă
+  punctaj = punctaj + adaugarepunctaj;
+  intrebaricomplete++;
+
+  // Dezactivează întrebarea
+  const intrebare = document.getElementById(idintrebare);
+  intrebare.classList.add("disabled-col");
+}
+
+function afiseazapr() {
+  // Verifică daca s-a raspuns la toate întrebările
+  if (intrebaricomplete < nrintrebari) {
+    alert("Nu ați răspuns la toate întrebările!");
+  }
+  else if (intrebaricomplete == nrintrebari) {
+  	// Evaluează
+    if (punctaj <= 5) {
+      document.getElementById("punctajafis").innerHTML = punctaj.toString();
+      rezultat = "Nu s-a detectat un consum inadecvat de alcool";
+      document.getElementById("rezultatafis").innerHTML = rezultat;
+    }
+    else if (punctaj >= 6) {
+      document.getElementById("punctajafis").innerHTML = punctaj.toString();
+      rezultat = "S-a detectat un consum inadecvat de alcool";
+      document.getElementById("rezultatafis").innerHTML = rezultat;
+    }
+
+  	//Ascunde afiseazapr și afișează pr
+  	document.getElementById("afiseazapr").remove();
+  	document.getElementById("pr").style.display = "block";
+  }
+}
+
+function refachestionarul() {
+  let confirmare = confirm("Sigur vreți să refaceți chestionarul? OK = DA; Cancel = NU");
+  if (confirmare) {
+    location.reload();
+  }
+}
